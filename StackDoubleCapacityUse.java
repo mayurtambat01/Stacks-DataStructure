93b86ed47d06f91b2c3409711ed976fafbacaf17
mayurtambat01 <mayurtambat01@gmail.com>
2024-06-10 12:39:43 +0530

    Update Stack

--- StackDoubleCapacityUse.java
+++ StackDoubleCapacityUse.java
@@ -1,15 +1,19 @@
 
 public class StackDoubleCapacityUse {
 
-    public static void main(String[] args) throws StackEmptyException {
+    public static void main(String[] args) {
         
         StackDoubleCapacity stack = new StackDoubleCapacity(10);
         for(int i=1;i <= 30;i++){
             stack.push(i);
         }
-        
+
         while (!stack.isEmpty()) {
-            stack.pop();
+            try {
+                System.out.println(stack.pop());
+            } catch (StackEmptyException e) {
+                // Never Reach Here
+            }
         }
     }
     
